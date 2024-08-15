6f184d7c3cb6be6bce06e066eba9dd63edf30512
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-16 01:59:51 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement('ul');
+// for(let i=0; i<3; i++){
+//     const li = document.createElement('li');
+//     li.textContent = (i+1).toString();
+//     ul.append(li);
+// }
+
+
+
+
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Abu Obida is the champion'; 
+
+document.body.appendChild(newHeader);
+
+
+
+
